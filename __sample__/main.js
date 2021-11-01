86ab08d139195537d1197b60ff875425223c5cd7
Frank_Wildfire <frankbae_wildfire@Frank-Wildfires-MacBook-Pro.local>
2021-11-01 09:48:05 -0700

    reverted back the console log from sample

--- __sample__/main.js
+++ __sample__/main.js
@@ -1,15 +1,1 @@
-var fullName = 'Frank Bae';
-var isCool = true;
-var totalPets = 0;
-var unknown = null;
-var never;
-console.log(fullName)
-console.log(isCool)
-console.log(totalPets)
-console.log(typeof fullName)
-console.log(typeof isCool)
-console.log(typeof totalPets)
-console.log(unknown)
-console.log(typeof unknown)
-console.log(never)
-console.log(typeof never)
+console.log('hi');
