7647d3e9a8025bb888c679dc8b419bc65f80671f
Frank_Wildfire <frankbae_wildfire@Frank-Wildfires-MacBook-Pro.local>
2021-11-01 10:16:35 -0700

    completed the assignment

--- js-files/js-primitives-and-variables/main.js
+++ js-files/js-primitives-and-variables/main.js
@@ -1,15 +1,38 @@
-var fullName = 'Frank Bae';
-var isCool = true;
-var totalPets = 0;
-var unknown = null;
-var never;
-console.log(fullName)
-console.log(isCool)
-console.log(totalPets)
-console.log(typeof fullName)
-console.log(typeof isCool)
-console.log(typeof totalPets)
-console.log(unknown)
-console.log(typeof unknown)
-console.log(never)
-console.log(typeof never)
+let width = 200;
+let height = 100;
+let area = width * height;
+console.log('value of area:', area)
+console.log('typeof area:', typeof area)
+
+let bill = 32;
+let payment = 40;
+let change = payment - bill;
+console.log('value of change:', change)
+console.log('typeof change:' + typeof change)
+
+let quizzes = 32;
+let midterm = 98;
+let final = 78;
+let average = (quizzes + midterm + final) / 3
+console.log('value of grade:', average)
+console.log('typeof grade:', typeof average)
+
+let firstName = 'Frank';
+let lastName = 'Bae';
+let fullName = firstName + ' ' +lastName
+console.log('value of fullName', fullName)
+console.log('typeof fullName', typeof fullName)
+
+let ph = 10;
+let isAcidic = ph < 7;
+console.log('value of isAcidic:', isAcidic)
+console.log('typeof isAcidic:', typeof isAcidic)
+
+let headCount = 22;
+let isSparta = headCount === 300
+console.log('value of isSparta:', isSparta)
+console.log('typeof isSparta:', typeof isSparta)
+
+let motto = fullName += ' is the goat'
+console.log('value of motto:', motto)
+console.log('type of:', typeof motto)
