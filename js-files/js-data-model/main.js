21e43e90b56b2c8bf1431e10f7b9719dffad265d
Frank_Wildfire <frankbae_wildfire@Frank-Wildfires-MacBook-Pro.local>
2021-11-01 11:14:45 -0700

    completed data model

---
+++ js-files/js-data-model/main.js
@@ -0,0 +1,25 @@
+// essentially im iterating through each object of the array 
+const orderHistory = [
+    {
+        orderPlaced: 'August 4, 2020',
+        total: '$34.00',
+        shipTo: 'JS Masher',
+        orderDetails: '1134-2323-23-213-445-4',
+        author: 'Alex',
+        delivery: 'Aug 8, 2020',
+        title: 'Javascript for impatient programmers',
+        return: 'Sep 7, 2020'
+    },
+    {
+        orderPlaced: 'August 9, 2020',
+        total: '$51.00',
+        shipTo: 'Chris',
+        orderDetails: '334-4863-23-989-445-4',
+        author: DragEvent. Axel,
+        delivery: 'Aug 16, 2020',
+        title: 'Timesless way of building',
+        return: 'Sep 10, 2020'
+    }
+]
+
+console.log('value of orderHistory', orderHistory)
