0e9e4f0f4475cfccb085a0277245fb228eaa5488
Frank Bae <frank.bae@wildfire-corp.com>
2021-11-03 15:50:52 -0700

    refactored and better organized the data model

--- js-files/js-data-model/main.js
+++ js-files/js-data-model/main.js
@@ -2,24 +2,41 @@
 const orderHistory = [
     {
         orderPlaced: 'August 4, 2020',
-        total: '$34.00',
         shipTo: 'JS Masher',
         orderDetails: '1134-2323-23-213-445-4',
-        author: 'Alex',
-        delivery: 'Aug 8, 2020',
-        title: 'Javascript for impatient programmers',
-        return: 'Sep 7, 2020'
+        item: [
+            {
+                name: 'Javascript for impatient programmers',
+                price: '$82.00',
+                author: 'Alex',
+                delivery: 'Aug 8, 2020',
+                trackingNumber: 'xxishjd9sdh23ksjd'
+            },
+            {
+                name: 'The best book',
+                price: '$10.00',
+                author: 'unknown',
+                delivery: 'Sep 01 2020',
+                trackingNumber: 'isajdoisajd0sadjsa0dj'
+            }
+        ],
+        total: '$92.00'
     },
     {
-        orderPlaced: 'August 9, 2020',
-        total: '$51.00',
-        shipTo: 'Chris',
+        orderPlaced: 'August 28, 2020',
+        shipTo: 'JS Other',
         orderDetails: '334-4863-23-989-445-4',
-        author: 'Chris',
-        delivery: 'Aug 16, 2020',
-        title: 'Time way of building',
-        return: 'Sep 10, 2020'
-    }
+        item: [
+            {
+                name: 'Time way of building',
+                price: '$32.00',
+                author: 'Chris',
+                delivery: 'Oct 8, 2020',
+                trackingNumber: '9821391h9823hr238h890'
+            },
+        ],
+        total: '$32.00'
+    },
 ]
 
 console.log('value of orderHistory', orderHistory)
